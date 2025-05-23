9aa610c0120caea0143b1a992b936da061b749cf
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-05-23 10:16:58 +0700

    feat: changes port

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
   res.send({ result: book });
 });
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
